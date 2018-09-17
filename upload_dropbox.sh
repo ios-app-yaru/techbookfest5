dbbc6305abd72922ab339eefb4a2a6e651efe60e
ryo-takahashi <ryo.takahashi.work@gmail.com>
2018-09-17 21:56:03 +0900

    add upload dropbox job on ci

---
+++ upload_dropbox.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ $# != 2 ]
+then
+	echo 'Usage: upload_dropbox.sh <file> <token>'
+	exit 1
+fi
+
+filename="book.pdf"
+
+echo "Upload $1 to $filename"
+
+curl --fail -X POST https://content.dropboxapi.com/2/files/upload \
+	--header "Authorization: Bearer $2" \
+	--header "Dropbox-API-Arg: {\"path\": \"/$filename\",\"mode\": \"add\",\"autorename\": true,\"mute\": false}" \
+	--header "Content-Type: application/octet-stream" \
+	--data-binary @$1 \
+	|| exit 1
+
+echo 
+echo "Success!"
